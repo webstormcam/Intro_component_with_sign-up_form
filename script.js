3b70fc8f8ca326a7174faab9aef759b9b2c9dfcc
Cameron Chandler <webstormcam@gmail.com>
2021-09-13 21:17:48 -0400

    Well i figured out how to toggle values

--- script.js
+++ script.js
@@ -1,42 +1,53 @@
-let fname = document.getElementById('fname')
-let fname_value = document.getElementById('fname').value
-
-let lname = document.getElementById('lname')
-let lname_value = document.getElementById('lname').value
 
 
-let email = document.getElementById('email')
-let email_value = document.getElementById('email').value
 
-let password = document.getElementById('password')
-let password_value = document.getElementById('password').value
-
-let error_icon = document.getElementsByClassName('error')
-let error_msg = document.getElementsByClassName('red')
-
-function failure_email(){
-    email.style.borderColor ='red'
-    email.style.borderWidth ='1px'
-    error_icon[2].style.display ='block'
-    error_msg[2].style.display ='block'
-}
 
 function validateForm(){
- if(fname_value ===''){
+    let fname = document.getElementById('fname')
+    let fname_value = document.getElementById('fname').value
+    
+    let lname = document.getElementById('lname')
+    let lname_value = document.getElementById('lname').value
+    
+    
+    let email = document.getElementById('email')
+    let email_value = document.getElementById('email').value
+    
+    let password = document.getElementById('password')
+    let password_value = document.getElementById('password').value
+    
+    let error_icon = document.getElementsByClassName('error')
+    let error_msg = document.getElementsByClassName('red')
+
+    function failure_email(){
+        email.style.borderColor ='red'
+        email.style.borderWidth ='1px'
+        error_icon[2].style.display ='block'
+        error_msg[2].style.display ='block'
+    }
+
+
+ if(fname_value.length ===0){
     fname.style.borderColor ='red'
     fname.style.borderWidth ='1px'
     error_icon[0].style.display ='block'
     error_msg[0].style.display ='block'
+ } else{
+   fname.style.borderWidth='1px'
+   fname.style.borderColor ='black'
+   error_icon[0].style.display ='none'
+   error_msg[0].style.display ='none'
  }
+
  
- if(lname_value ===''){
+ if(lname_value.length ===0){
     lname.style.borderColor ='red'
     lname.style.borderWidth ='1px'
     error_icon[1].style.display ='block'
     error_msg[1].style.display ='block'
  }
 
- if(email_value ===''){
+ if(email_value.length ===0){
     failure_email();
  } else if( email_value.includes('@') === false){
     failure_email();
@@ -44,12 +55,16 @@ function validateForm(){
     failure_email();
  }
 
- if(password_value ===''){
+ if(password_value.length ===0){
     password.style.borderColor ='red'
     password.style.borderWidth ='1px'
     error_icon[3].style.display ='block'
     error_msg[3].style.display ='block'
  }
 
-
+ else if(fname_value.length>3 && lname_value.length>3&& email_value.includes('@') === true && email_value.length>4 && password_value.length>3){
+    document.getElementById("myForm").submit();
+ }
+ 
 }
+
